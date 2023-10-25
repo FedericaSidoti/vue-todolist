735f3bbad5132b7e724f78623cef6301bb359183
FedericaSidoti <federicasidoti21@gmail.com>
2023-10-25 14:15:03 +0200

    COMMIT- first non-dynamic stamp

--- js/app.js
+++ js/app.js
@@ -3,7 +3,20 @@ const createApp = Vue.createApp
 createApp({
     data() {
         return {
-
+            tasks: [
+                {
+                    text: 'Fare il nuovo esercizio',
+                    done: false,
+                },
+                {
+                    text: 'Riguardare le repo dei livecoding',
+                    done: false,
+                },
+                {
+                    text: 'Piangere coi colleghi',
+                    done: true,
+                }
+            ]
         }
     },
     methods : {
