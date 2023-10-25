ba2b3d1500711c003eda48b13bdb5752a9b97668
FedericaSidoti <federicasidoti21@gmail.com>
2023-10-25 14:01:11 +0200

    FIRSTCOMMIT- html, css and vue basis

---
+++ js/app.js
@@ -0,0 +1,15 @@
+const createApp = Vue.createApp
+
+createApp({
+    data() {
+        return {
+
+        }
+    },
+    methods : {
+
+    },
+    mounted() {
+        console.log('ok')
+    }
+}).mount('#app')
